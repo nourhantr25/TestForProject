5f6f0e7abaa12c73350d847f420e038a505491d4
nourhantr25 <121582035+nourhantr25@users.noreply.github.com>
2022-12-28 23:58:56 +0200

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,18 @@
+# TestForProject
+# Program to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+
+yy = 2014  # year
+mm = 11    # month
+
+u="user25@@tra.gov.eg"
+p="1234AAAA"
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
